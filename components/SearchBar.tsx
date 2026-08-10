848098796955259a60ef08c6d2640f3e39bb6cc6
mohsinabbas123 <abbasmohsin789@gmail.com>
2026-08-10 06:18:57 +0500

    implement a searchBar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,17 @@
+import { icons } from "@/constants/icons";
+import { Image, View } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View className="flex-row items-center  bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
