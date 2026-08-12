fbb09f16ea4983d36546cc17e31fd9b1822ce400
mohsinabbas123 <abbasmohsin789@gmail.com>
2026-08-12 10:08:28 +0500

    add  a commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
@@ -6,18 +5,14 @@ import { Image, ScrollView, View } from "react-native";
 const Index = () => {
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
+      <Image source={images.bg} className=" w-full absolute z-0  " />
 
       <ScrollView
         className="flex-1 px-5"
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
-        <View className="flex-1 mt-5">
-          <SearchBar />
-        </View>
+        <Image source={icons.logo} className="mt-20 mb-5 mx-auto  w-12 h-10" />
       </ScrollView>
     </View>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,9 @@
-import { icons } from "@/constants/icons";
-import { Image, View } from "react-native";
+import { Text, View } from "react-native";
 
 const SearchBar = () => {
   return (
-    <View className="flex-row items-center  bg-dark-200 rounded-full px-5 py-4">
-      <Image
-        source={icons.search}
-        className="size-5"
-        resizeMode="contain"
-        tintColor="#ab8bff"
-      />
+    <View>
+      <Text>SearchBar</Text>
     </View>
   );
 };
