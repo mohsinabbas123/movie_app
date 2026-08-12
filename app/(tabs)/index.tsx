609ae7883bb7e217682338469517bdf717816aa4
mohsinabbas123 <abbasmohsin789@gmail.com>
2026-08-12 10:50:42 +0500

    implement a search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 const Index = () => {
+  const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className=" w-full absolute z-0  " />
@@ -13,6 +16,13 @@ const Index = () => {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="mt-20 mb-5 mx-auto  w-12 h-10" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
       </ScrollView>
     </View>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
 
-const SearchBar = () => {
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
     </View>
   );
 };
